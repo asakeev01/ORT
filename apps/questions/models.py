ed9ee927351aa940d8f3ee39bb5adae6a3ff9acb
asakeev01 <aidar.asakeev@gmail.com>
2020-06-09 15:50:31 +0600

    second push

--- apps/questions/models.py
+++ apps/questions/models.py
@@ -1,21 +1,30 @@
 from django.db import models
-
+RIGHT_ANSWER_CHOICE = [
+    ('а_вариант', 'А_ВАРИАНТ'),
+    ('б_вариант', 'Б_ВАРИАНТ'),
+    ('в_вариант', 'В_ВАРИАНТ'),
+    ('г_вариант', 'Г_ВАРИАНТ'),
+    ('д_вариант', 'Д_ВАРИАНТ'),
+    ]
 
 class Introduction(models.Model):
-    number = models.PositiveIntegerField()
-    name = models.CharField(max_length = 255)
-    description = models.TextField()
-    score = models.PositiveIntegerField(default = 0)
+    номер = models.PositiveIntegerField()
+    имя = models.CharField(max_length = 255)
+    введение = models.TextField()
+    балл = models.PositiveIntegerField(default = 0)
 
 class Question(models.Model):
-    number = models.PositiveIntegerField()
-    condition = models.TextField()
-    introduction = models.ForeignKey(Introduction, on_delete = models.CASCADE, related_name = 'question')
-    first_choice = models.CharField(max_length = 100)
-    second_choice = models.CharField(max_length = 100)
-    third_choice = models.CharField(max_length = 100)
-    fourth_choice = models.CharField(max_length = 100)
-    right_choice = models.CharField(max_length = 100)
+    номер = models.PositiveIntegerField()
+    условие= models.TextField()
+    введение = models.ForeignKey(Introduction, on_delete = models.CASCADE, related_name = 'question')
+    a_вариант = models.CharField(max_length = 100, null = True, blank = True)
+    б_вариант = models.CharField(max_length = 100, null = True, blank = True)
+    в_вариант = models.CharField(max_length = 100, null = True, blank = True)
+    г_вариант = models.CharField(max_length = 100, null = True, blank = True)
+    д_вариант = models.CharField(max_length = 100, null = True, blank = True)
+    правильный_вариант = models.CharField(max_length = 100, choices = RIGHT_ANSWER_CHOICE,
+        default = a_вариант)
+    объяснение = models.TextField(null = True, blank = True)
     
 
 
--- apps/questions/views.py
+++ apps/questions/views.py
@@ -8,13 +8,18 @@ def tasks_list(request):
     return render(request, 'tasks/task.html', locals())
 
 def questions_list(request, pk):
+    answered = False
     task = Introduction.objects.get(pk = pk)
     all_questions = task.question.all()
+    last_question = task.question.last()
     if request.method == 'POST':
-        for i in range(1, 3):
+        answered = True 
+        for i in range(1, last_question.номер + 1):
             answers = request.POST.get(str(i))
-            print(i)
-            print(answers)
+            current_question = task.question.get(номер = i)
+            объяснение = current_question.объяснение
+            if answers == current_question.правильный_вариант:
+                task.балл += 1
         return render(request, 'tasks/question.html', locals())
     else:
         return render(request, 'tasks/question.html', locals())
